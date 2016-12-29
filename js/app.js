78c7b69ed963a5836a5d9ed6249d9bf61594d179
Jasson Rdz <ja7rodal@gmail.com>
2016-12-28 21:41:59 -0500

    ngRoute and templates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 app.controller('ContactController', ['$scope', function($scope) {
     $scope.text = "Works";
 }]);
-    
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when("/", {
+        templateUrl : "index.html"
+        })
+    .when("/edit", {
+        templateUrl: "template/edit.html"      
+        })
+    .otherwise({
+    template : "contact.html"
+        });
+});
+
