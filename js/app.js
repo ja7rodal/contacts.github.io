9ea1675e68c4dc91873fa6a41ba4b20df8c3b77b
Jasson Rdz <ja7rodal@gmail.com>
2017-01-02 17:33:47 -0500

    server with gulp

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 var config = {
-    apiKey: "AIzaSyBsVzHAEPXrhcbo3qKcOqHadFEk58VhsI0",
-    authDomain: "brilliant-torch-9248.firebaseapp.com",
-    databaseURL: "https://brilliant-torch-9248.firebaseio.com",
-    storageBucket: "brilliant-torch-9248.appspot.com",
-    messagingSenderId: "936020393527"
+	apiKey: "AIzaSyBsVzHAEPXrhcbo3qKcOqHadFEk58VhsI0",
+  authDomain: "brilliant-torch-9248.firebaseapp.com",
+  databaseURL: "https://brilliant-torch-9248.firebaseio.com",
+  storageBucket: "brilliant-torch-9248.appspot.com",
+  messagingSenderId: "936020393527"
 };
 firebase.initializeApp(config);
 
 var app = angular.module('app', ['ngRoute', 'firebase']);
-app.controller('ContactController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-    $scope.text = "Works";
-    var ref = firebase.database().ref("contacts");
-    // create a synchronized array
-    $scope.contacts = $firebaseArray(ref);
-    console.info($scope.contacts);
-    // add new items to the array
-    // the message is automatically added to our Firebase database!
-    $scope.addContact = function() {
-        $scope.contacts.$add({
-            name: $scope.named,
-            phone: $scope.phone
-        });
-        $scope.named = '';
-        $scope.phone = '';
-    };
+app.controller('ContactController', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
+	$scope.text = "Works";
+	var ref = firebase.database().ref("contacts");
+	// create a synchronized array
+	$scope.contacts = $firebaseArray(ref);
+	console.info($scope.contacts);
+	// add new items to the array
+	// the message is automatically added to our Firebase database!
+	$scope.addContact = function () {
+		$scope.contacts.$add({
+			name: $scope.named,
+			phone: $scope.phone
+		});
+		$scope.named = '';
+		$scope.phone = '';
+	};
 }]);
