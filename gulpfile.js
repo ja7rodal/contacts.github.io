fc3782396e29629694760ab7797dae2985cac76d
Jasson Rdz <ja7rodal@gmail.com>
2017-01-06 17:40:43 -0500

    minify css and js with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 ////////////////////////////////////
+var minify = require('gulp-minify');
 var gulp = require('gulp'),
   connect = require('gulp-connect');
+
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', function() {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
  
 gulp.task('webserver', function(){
 	connect.server({
@@ -17,5 +26,18 @@ gulp.task('html', function () {
 gulp.task('watch', function () {
   gulp.watch(['./app/*.html'], ['html']);
 });
+
+gulp.task('compress', function() {
+  gulp.src('lib/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('dist'))
+});
  
-gulp.task('default', ['webserver', 'watch']);
+gulp.task('default', ['minify-css', 'webserver', 'watch', 'compress',]);
